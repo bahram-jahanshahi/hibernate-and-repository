8b709ead700e1089f370b2f597c7062b8ce4f8c6
Bahram Jahanshahi <jahanshahi@amnpardaz.com>
2019-12-26 12:22:11 +0330

    Initial

--- src/test/java/ir/mctab/java32/hibernate/projects/hibernateandrepository/repositpries/TestLaptopRepository.java
+++ src/test/java/ir/mctab/java32/hibernate/projects/hibernateandrepository/repositpries/TestLaptopRepository.java
@@ -3,11 +3,13 @@ package ir.mctab.java32.hibernate.projects.hibernateandrepository.repositpries;
 import ir.mctab.java32.hibernate.projects.hibernateandrepository.domain.Laptop;
 import ir.mctab.java32.hibernate.projects.hibernateandrepository.repositories.LaptopRepository;
 
+import java.util.stream.Stream;
+
 public class TestLaptopRepository {
     static LaptopRepository laptopRepository = LaptopRepository.getInstance();
 
     public static void main(String[] args) {
-        for (int i = 0; i < 50; i++) {
+        /*for (int i = 0; i < 50; i++) {
             laptopRepository
                     .save(
                             new Laptop(
@@ -16,6 +18,13 @@ public class TestLaptopRepository {
                                     "Core i9 8MB 16G 4G 1T SSD"
                             )
                     );
-        }
+        }*/
+        laptopRepository
+                .findAll()
+                .stream()
+                // .map(laptop -> laptop.getHardwareConfig())
+                .filter(laptop -> laptop.getHardwareConfig().contains("i9"))
+                .flatMap(laptop -> Stream.of(laptopRepository.findById(laptop.getId()).getTitle()))
+                .forEach(System.out::println);
     }
 }
